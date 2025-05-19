b10305bed9544139809495f66f1178b92333c51b
vanessa94ps <vanessapessoas.94@gmail.com>
2025-05-19 12:01:38 -0300

    Create os_jovens_vingadores.sh
    teste jovens vingadores

---
+++ os_jovens_vingadores.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Lista dos novos Vingadores"
+
+echo "Homem Aranha, Gaviã Arqueira, Miss Marvel,Coração de Ferro, Wiccano" 
+
+echo "Esperando a Marvel nos apresentar novos nomes para a saga"
+
+calc.exe
